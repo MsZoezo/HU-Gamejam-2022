cd04e342cc55dee4ff81ca76807e0cfe2039fc4e
Jaap <jareerink@gmail.com>
2022-04-28 11:52:49 +0200

    added unity project

---
+++ JamOS/Assets/scripts/Timescript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class Timescript : MonoBehaviour
+{
+    private TextMeshProUGUI currentTime;
+
+
+    private void Start()
+    {
+        currentTime = transform.GetComponent<TextMeshProUGUI>();    
+    }
+
+    private void Update()
+    {
+        currentTime.text = DateTime.Now.ToShortTimeString();
+    }
+
+
+}
